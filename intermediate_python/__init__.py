91d61d6be86f960369ce20d84a48ef00fe75094a
雷圳 <leizhen8080@foxmail.com>
2019-01-05 09:27:54 +0800

    ok

---
+++ intermediate_python/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+# @Time    : 2019/1/5 09:27
+# @Author  : Lei Zhen
+# @Contract: leizhen8080@gmail.com
+# @Site    : http://www.leizhen.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃  ☃    ┃
+            ┃ ┳┛  ┗┳ ┃
+            ┃      ┻  ┃
+            ┗━┓     ┏━┛
+              ┃     ┗━━━┓
+              ┃ 神兽保佑 ┣┓
+              ┃ 永无BUG┏┛
+              ┗┓┓┏━┳┓┏┛
+               ┃┫┫ ┃┫┫
+               ┗┻┛ ┗┻┛
+"""
