a36285527e953bce33b7419a5964e6dfb3261d43
leizhen <1027224454@qq.com>
2019-11-11 14:05:22 +0800

    添加对商品橱窗的简单解析

---
+++ douyin/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+# @Time    : 19/11/8 13:50
+# @Author  : Lei Zhen
+# @Contract: leizhen8080@gmail.com
+# @Site    : http://www.leizhen.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃  ☃    ┃
+            ┃ ┳┛  ┗┳ ┃
+            ┃      ┻  ┃
+            ┗━┓     ┏━┛
+              ┃     ┗━━━┓
+              ┃ 神兽保佑 ┣┓
+              ┃ 永无BUG┏┛
+              ┗┓┓┏━┳┓┏┛
+               ┃┫┫ ┃┫┫
+               ┗┻┛ ┗┻┛
+"""
